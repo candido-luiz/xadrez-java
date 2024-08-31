344bdaeb014f54be1fef47ff447eba8767a7337c
candido-mint <candidoluiz097@gmail.com>
2024-08-31 11:01:38 -0300

    feat(MoveValidation): Add valitation for possible moves

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -12,4 +12,21 @@ public abstract class Piece {
 		return board;
 	}
 	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] matrix = possibleMoves();
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				if(matrix[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
